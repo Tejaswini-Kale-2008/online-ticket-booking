e28285272f027219206415408138d0b29a6cb965
Tejaswini-Kale-2008 <kaletejaswini36@gmail.com>
2025-07-15 17:31:45 +0530

    Create JPAUtil.java

---
+++ src/main/java/ticketbooking/dao/JPAUtil.java
@@ -0,0 +1,24 @@
+package ticketbooking.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory emf;
+    try{
+           emf = Persistence.createEntityManagerFactory("TicketBooking");
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void shutdown() {
+         {
+            emf.close();
+        }
+    }
+}

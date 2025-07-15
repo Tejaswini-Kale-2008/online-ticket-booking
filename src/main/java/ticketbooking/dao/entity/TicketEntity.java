9a08ab0dc19204ec061c42c120d870be5f463d5d
Tejaswini-Kale-2008 <kaletejaswini36@gmail.com>
2025-07-15 17:22:03 +0530

    Create TicketEntity.java

---
+++ src/main/java/ticketbooking/dao/entity/TicketEntity.java
@@ -0,0 +1,49 @@
+ackage ticketbooking.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tickets")
+
+public class TicketEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String passengerName;
+    private String destination;
+    private double price;
+
+    public TicketEntity() {}
+
+    public TicketEntity(String passengerName, String destination, double price) {
+        this.passengerName = passengerName;
+        this.destination = destination;
+        this.price = price;
+    }
+
+    public int getId() { return id; }
+    public String getPassengerName() { return passengerName; }
+    public String getDestination() { return destination; }
+    public double getPrice() { return price; }
+
+    public void setPassengerName(String name)
+    { 
+    this.passengerName = name; 
+    }
+    public void setDestination(String dest) 
+    {
+    this.destination = dest;
+    }
+    public void setPrice(double price)
+    {
+    this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Ticket[id=%d, name='%s', destination='%s', price=%.2f]",
+                id, passengerName, destination, price);
+    }
+}
